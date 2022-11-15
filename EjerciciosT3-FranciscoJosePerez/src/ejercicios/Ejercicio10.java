24de699729d9659e46cd7c14630be8f96f610e58
fjperez1 <francisco.perez@iesnervion.es>
2022-11-15 14:39:53 +0100

    a?

--- EjerciciosT3-FranciscoJosePerez/src/ejercicios/Ejercicio10.java
+++ EjerciciosT3-FranciscoJosePerez/src/ejercicios/Ejercicio10.java
@@ -18,38 +18,37 @@ public class Ejercicio10 {
 		n_cif++;
 		}
 		
-		for (int dig_izq=1;dig_izq<=n_cif;dig_izq--) {
-			if (n_intro>9) {
-				if (dig_izq<n_cif) {
-					temp1=(n_intro/(10^dig_izq))%10;
-				} else
-					temp1=(n_intro/(10^dig_izq));
-			} else
-				temp1=n_intro;
-		}
-		for (int dig_der=1;dig_der<=(n_cif*2);dig_der++) {
-			if (n_intro>9) {
-				if (dig_der<n_cif) {
-					temp1=(n_intro/(10^dig))%10;
-				} else
-					temp1=(n_intro/(10^dig));
-			} else
-				temp2=n_intro;
-		}
-		
-		
-		
-		
 		if (n_cif%2==0) {
-			for(int cont=n_cif/2;cont>0;cont-=2) {
+			
+			if (n_cif<2) {
+				System.out.println("¡El número introducido es capicúa! aunque deberías probar con uno de más cifras");
+				break;
+				
+			} else if (n_cif==2) {
+				
+				for (int dig_izq=1;dig_izq<=n_cif/2;dig_izq++) {
+			
+					
+				}
+			} else if (n_cif>2) {
 				
 			}
-			for(int cont=n_cif/2;cont>0;cont+=2) {
-				
-			}
+			
 		} else {
 			
+			for (int dig_der=1;dig_der<=(n_cif*2);dig_der++) {
+				if (n_intro>9) {
+					if (dig_der<n_cif) {
+						temp1=(n_intro/(10^dig))%10;
+					} else
+						temp1=(n_intro/(10^dig));
+				} else
+					temp2=n_intro;
+			}
+			
 		}
+		
+		
 	}
 
 }
--- EjerciciosT3-FranciscoJosePerez/src/ejercicios/a.java
+++ EjerciciosT3-FranciscoJosePerez/src/ejercicios/a.java
@@ -5,7 +5,14 @@ public class a {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-int b=(3265/100)%10;
+int b=(9578/1000)%10;
+
+/*
+1a cifra -> 8
+2a cifra -> 7
+3a cifra -> 5
+4a cifra -> 9
+*/
 System.out.println(b);
 	}
 
