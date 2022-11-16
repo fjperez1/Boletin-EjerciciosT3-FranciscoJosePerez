b6f87b6ed9fbcc4c435ae75b3b69f03aca5c0cc3
fjperez1 <francisco.perez@iesnervion.es>
2022-11-16 14:39:54 +0100

    aa

--- EjerciciosT3-FranciscoJosePerez/src/ejercicios/Ejercicio10.java
+++ EjerciciosT3-FranciscoJosePerez/src/ejercicios/Ejercicio10.java
@@ -7,48 +7,23 @@ public class Ejercicio10 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int n_intro,n_cif=0, temp1;
-		boolean capicua="true";
+		int n_intro=20, temp1, dobl=0, sav=0;
 		Scanner lectura=new Scanner(System.in);
 		
 		System.out.println("INPUT: Introduce un número para conocer cuántas cifras tiene");
 		n_intro=lectura.nextInt();
+		sav=n_intro;
 		
-		for (int cif=n_intro;cif>0;cif/=10) {
-		n_cif++;
+		while (n_intro>0) {
+			temp1=n_intro%10;
+			dobl=dobl*10+temp1;
+			n_intro/=10;
 		}
 		
-		if (n_cif%2==0) {
-			
-			if (n_cif<2) {
-				System.out.println("¡El número introducido es capicúa! aunque deberías probar con uno de más cifras");
-				break;
-				
-			} else if (n_cif==2) {
-				
-				for (int dig_izq=1;dig_izq<=n_cif/2;dig_izq++) {
-			
-					
-				}
-			} else if (n_cif>2) {
-				
-			}
-			
-		} else {
-			
-			for (int dig_der=1;dig_der<=(n_cif*2);dig_der++) {
-				if (n_intro>9) {
-					if (dig_der<n_cif) {
-						temp1=(n_intro/(10^dig))%10;
-					} else
-						temp1=(n_intro/(10^dig));
-				} else
-					temp2=n_intro;
-			}
-			
-		}
-		
-		
-	}
+		if (sav==dobl) {
+			System.out.println("OUTPUT: El número introducido es capicúa");
+		} else
+			System.out.println("OUTPUT: El número introducido no es capicúa");
 
+}
 }
